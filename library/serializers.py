2b09640b74c1f97ea5085191fd334a4997cde20c
Ekhidna <ekhidna@disroot.org>
2025-10-28 04:45:40 +0700

    Инициализация serializers.py. Настроены сериалайзеры для моделей.

---
+++ library/serializers.py
@@ -0,0 +1,15 @@
+# library/serializers.py
+from rest_framework import serializers
+from .models import Author, Book
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+class BookSerializer(serializers.ModelSerializer):
+    author_name = serializers.CharField(source='author.__str__', read_only=True)
+
+    class Meta:
+        model = Book
+        fields = '__all__'
