b3659f061c348536302809df4bb78b272917c752
Ekhidna <ekhidna@disroot.org>
2025-10-28 04:42:22 +0700

    Модели Author, Book зарегистрированы в админке

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,12 @@
+# library/admin.py
 from django.contrib import admin
+from .models import Author, Book
 
-# Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year', 'publisher', 'category')
+    list_filter = ('category', 'genre')
