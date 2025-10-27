5168dfff501fc05dcd5f1f0a68a29526207b36fb
Ekhidna <ekhidna@disroot.org>
2025-10-28 04:41:06 +0700

    Добавлены модели Author, Book c проверкой уникальности

--- library/models.py
+++ library/models.py
@@ -1,3 +1,40 @@
+# library/models.py
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    biography = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    publication_year = models.IntegerField()
+    genre = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, choices=[
+        ('fiction', 'Художественная литература'),
+        ('textbook', 'Учебник'),
+    ])
+    publisher = models.CharField(max_length=100)
+    cover_image = models.ImageField(upload_to='covers/', blank=True, null=True)
+    book_file = models.FileField(upload_to='books/')
+
+    def __str__(self):
+        return self.title
+
+    def clean(self):
+        if Book.objects.filter(
+            title=self.title,
+            author=self.author,
+            publication_year=self.publication_year,
+            publisher=self.publisher
+        ).exclude(pk=self.pk).exists():
+            raise ValidationError("Дубликат книги запрещён.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
